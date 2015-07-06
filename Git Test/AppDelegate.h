2e42aafc5ea7fe69edcd696cd2cd1688c560608a
Mobisys <mobisys@Mobisyss-iMac.local>
2015-07-05 23:08:22 -0700

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Mobisys on 7/5/15.
+//  Copyright (c) 2015 Mobisys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Mobisys on 7/5/15.
+//  Copyright (c) 2015 Mobisys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
