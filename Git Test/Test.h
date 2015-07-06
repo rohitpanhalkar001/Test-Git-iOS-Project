76b66657bb9a86ea7853dc626800b2f2e67c1577
Mobisys <mobisys@Mobisyss-iMac.local>
2015-07-05 23:31:52 -0700

    add Test Class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Mobisys on 7/5/15.
+//  Copyright (c) 2015 Mobisys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Mobisys on 7/5/15.
+//  Copyright (c) 2015 Mobisys. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
